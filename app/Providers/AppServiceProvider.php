45867f8dc39eb22d67dbc932daeb33ca0e53a50d
MarioCass10 <mario.castillo.ceb@immune.institute>
2023-03-08 10:21:18 +0100

    fix Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,13 @@
-<A?php
+<?php
 
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-...
+
 class AppServiceProvider extends ServiceProvider
 {
-...
+
     public function boot()
     {
         Schema::defaultStringLength(191);
